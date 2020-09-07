6f521b9ce835089b495db3d211275c69572dffd6
Joshua Chapman <joshua.samuel.chapman@protonmail.com>
2020-09-07 18:03:16 +0200

    Implemented opening the database

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use rusqlite::{Connection, Result};
+
+const DB_PATH: &str = "flag.db";
+const FLAG_TABLE: &str =
+    "CREATE TABLE flags IF NOT EXISTS (flag TEXT PRIMARY KEY, group INT NOT NULL, sent BOOLEAN NOT NULL DEFAULT 0)";
+
+#[derive(Debug)]
+struct Flag<'a> {
+    flag: &'a str,
+    group: u8,
+    sent: bool,
+}
+
+fn setup() {}
+
+fn main() -> Result<()> {
+    let db = Connection::open(DB_PATH)?;
+    println!("{}", db.is_autocommit());
+    Ok(())
 }
