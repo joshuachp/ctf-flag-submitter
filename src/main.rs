a8205c6a9c9f8de2f35237f03d91a8c9ca132587
Joshua Chapman <joshua.samuel.chapman@protonmail.com>
2020-09-11 12:03:16 +0200

    Added set flag to sent

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use reqwest;
 use rusqlite::params;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use tokio::time;
 
 const DB_PATH: &str = "flag.db";
@@ -10,6 +10,8 @@ const SLEEP_TIME: u8 = 10;
 const FLAGS_PER_SECOND: u8 = 25;
 // Table
 const FLAG_TABLE: &str = "CREATE TABLE IF NOT EXISTS flags (flag TEXT PRIMARY KEY, group_id INT NOT NULL, sent BOOLEAN NOT NULL DEFAULT 0)";
+const SELECT_UNSENT: &str = "SELECT flag, group_id, sent FROM flags WHERE sent = 0";
+const UPDATE_SENT: &str = "UPDATE flags SET sent = 1 WHERE flag = ?";
 
 #[derive(Debug)]
 struct Flag {
@@ -24,7 +26,7 @@ fn setup(db: &rusqlite::Connection) -> rusqlite::Result<()> {
 }
 
 fn get_unsent_flags(db: &rusqlite::Connection) -> Result<Vec<Arc<Flag>>, rusqlite::Error> {
-    let mut prepare = db.prepare("SELECT flag, group_id, sent FROM flags WHERE sent = 0")?;
+    let mut prepare = db.prepare(SELECT_UNSENT)?;
     let flags: Vec<Arc<Flag>> = prepare
         .query_map(params![], |row| {
             Ok(Flag {
@@ -39,7 +41,13 @@ fn get_unsent_flags(db: &rusqlite::Connection) -> Result<Vec<Arc<Flag>>, rusqlit
     Ok(flags)
 }
 
-async fn send_single_flag(flag: Arc<Flag>) -> Result<reqwest::Response, reqwest::Error> {
+// TODO: Use batch insert for all sent flags
+fn set_sent_flag(db: &rusqlite::Connection, flag: &Flag) -> rusqlite::Result<()> {
+    db.execute(UPDATE_SENT, params![flag.flag])?;
+    Ok(())
+}
+
+async fn send_single_flag(flag: &Flag) -> Result<reqwest::Response, reqwest::Error> {
     println!("[SEND] flag: {} group: {}", flag.flag, flag.group);
     let client = reqwest::Client::new();
     let parameters = [("team_token", TEAM_TOKEN), ("flag", &flag.flag)];
@@ -47,26 +55,39 @@ async fn send_single_flag(flag: Arc<Flag>) -> Result<reqwest::Response, reqwest:
     Ok(res)
 }
 
-async fn send_flags_with_throttle(flags: &Vec<Arc<Flag>>) {
+fn check_response(_res: reqwest::Response) -> bool {
+    // TODO: Check response
+    false
+}
+
+async fn send_flags_with_throttle(db: &Arc<Mutex<rusqlite::Connection>>, flags: &Vec<Arc<Flag>>) {
     let mut interval = time::interval(time::Duration::from_secs(1));
     for chunk in flags.chunks(FLAGS_PER_SECOND as usize) {
         interval.tick().await;
         for flag in chunk {
-            let f = Arc::clone(flag);
-            println!("Start thread {:?}", f);
-            tokio::task::spawn(async { send_single_flag(f).await });
+            let db = Arc::clone(db);
+            let flag = Arc::clone(flag);
+            tokio::task::spawn(async move {
+                if let Ok(res) = send_single_flag(&flag).await {
+                    if check_response(res) {
+                        if let Err(err) = set_sent_flag(&db.lock().unwrap(), &flag) {
+                            eprintln!("[ERROR][SET] {}", err);
+                        };
+                    }
+                }
+            });
         }
     }
 }
 
-async fn main_loop(db: &rusqlite::Connection) {
+async fn main_loop(db: &Arc<Mutex<rusqlite::Connection>>) {
     let mut interval = tokio::time::interval(tokio::time::Duration::from_secs(SLEEP_TIME as u64));
     loop {
-        match get_unsent_flags(&db) {
+        match get_unsent_flags(&db.lock().unwrap()) {
             Ok(flags) => {
-                send_flags_with_throttle(&flags).await;
+                send_flags_with_throttle(db, &flags).await;
             }
-            Err(err) => eprintln!("{}", err),
+            Err(err) => eprintln!("[ERROR][GET] {}", err),
         }
         interval.tick().await;
     }
@@ -74,8 +95,8 @@ async fn main_loop(db: &rusqlite::Connection) {
 
 #[tokio::main]
 async fn main() -> rusqlite::Result<()> {
-    let db = rusqlite::Connection::open(DB_PATH)?;
-    setup(&db)?;
+    let db = Arc::new(Mutex::new(rusqlite::Connection::open(DB_PATH)?));
+    setup(&db.lock().unwrap())?;
     main_loop(&db).await;
     Ok(())
 }
