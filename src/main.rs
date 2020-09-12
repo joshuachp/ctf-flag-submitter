a23ad00b19c1553fd98a7f5ec84a684b8989a77e
Joshua Chapman <joshua.samuel.chapman@protonmail.com>
2020-09-12 15:41:44 +0200

    Added toml config file for send flags

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use clap::{crate_description, crate_name, crate_version, value_t_or_exit, App, Arg};
-use reqwest;
-use rusqlite::params;
+use reqwest::{Client, Response};
+use rusqlite::{params, Connection};
+use serde::Deserialize;
 use std::collections::HashSet;
+use std::fs::File;
+use std::io::prelude::*;
 use std::sync::{Arc, Mutex};
 use tokio::task::{spawn, JoinHandle};
 use tokio::time::{interval, Duration};
+use toml;
 
 // Table
 const FLAG_TABLE: &str = "CREATE TABLE IF NOT EXISTS flags (id INTEGER PRIMARY KEY, flag TEXT NOT NULL UNIQUE, group_id INT NOT NULL, sent BOOLEAN NOT NULL DEFAULT 0)";
@@ -19,6 +23,7 @@ struct Flag {
     sent: bool,
 }
 
+#[derive(Debug, Deserialize)]
 struct Config {
     db_path: String,
     server_url: String,
@@ -31,6 +36,14 @@ fn config() -> Arc<Config> {
     let matches = App::new(crate_name!())
         .version(crate_version!())
         .about(crate_description!())
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("CONFIG_PATH")
+                .help("Path to the Toml config file")
+                .takes_value(true),
+        )
         .arg(
             Arg::with_name("db_path")
                 .short("D")
@@ -47,7 +60,7 @@ fn config() -> Arc<Config> {
                 .value_name("SERVER_URL")
                 .help("URL of the POST request for challenge server")
                 .takes_value(true)
-                .required(true),
+                .required_unless("config"),
         )
         .arg(
             Arg::with_name("team_token")
@@ -56,7 +69,7 @@ fn config() -> Arc<Config> {
                 .value_name("TEAM_TOKEN")
                 .help("Team token to score points")
                 .takes_value(true)
-                .required(true),
+                .required_unless("config"),
         )
         .arg(
             Arg::with_name("check_interval")
@@ -78,6 +91,35 @@ fn config() -> Arc<Config> {
         )
         .get_matches();
 
+    if matches.is_present("config") {
+        let config_file =
+            read_config_file(matches.value_of("config").unwrap()).unwrap_or_else(|err| {
+                eprintln!("[ERROR][CONFIG] {}", err);
+                panic!("read_config_file");
+            });
+        let mut config: Config = toml::from_str(&config_file).unwrap_or_else(|err| {
+            eprintln!("[ERROR][CONFIG] {}", err);
+            panic!("toml::from_str");
+        });
+
+        if matches.is_present("db_path") {
+            config.db_path = String::from(matches.value_of("db_path").unwrap());
+        }
+        if matches.is_present("server_url") {
+            config.server_url = String::from(matches.value_of("server_url").unwrap());
+        }
+        if matches.is_present("team_token") {
+            config.team_token = String::from(matches.value_of("team_token").unwrap());
+        }
+        if matches.is_present("check_interval") {
+            config.check_interval = value_t_or_exit!(matches.value_of("check_interval"), u8);
+        }
+        if matches.is_present("flags_quota") {
+            config.flags_quota = value_t_or_exit!(matches.value_of("flags_quota"), u8);
+        }
+
+        return Arc::new(config);
+    }
     Arc::new(Config {
         db_path: String::from(matches.value_of("db_path").unwrap()),
         server_url: String::from(matches.value_of("server_url").unwrap()),
@@ -87,13 +129,20 @@ fn config() -> Arc<Config> {
     })
 }
 
-fn setup(db: &rusqlite::Connection) -> rusqlite::Result<()> {
+fn read_config_file(path: &str) -> std::io::Result<String> {
+    let mut file = File::open(path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
+}
+
+fn setup(db: &Connection) -> rusqlite::Result<()> {
     // Create table flag
     db.execute(FLAG_TABLE, params![])?;
     Ok(())
 }
 
-fn get_unsent_flags(db: &rusqlite::Connection) -> Result<Vec<Arc<Flag>>, rusqlite::Error> {
+fn get_unsent_flags(db: &Connection) -> Result<Vec<Arc<Flag>>, rusqlite::Error> {
     // Prepare query for select unsent flags
     let mut prepare = db.prepare(SELECT_UNSENT)?;
     // Map return to Flag struct
@@ -112,7 +161,7 @@ fn get_unsent_flags(db: &rusqlite::Connection) -> Result<Vec<Arc<Flag>>, rusqlit
     Ok(flags)
 }
 
-fn set_sent_flags(db: &rusqlite::Connection, id: i64) -> rusqlite::Result<()> {
+fn set_sent_flags(db: &Connection, id: i64) -> rusqlite::Result<()> {
     // Set the flag with the id to sent
     db.execute(UPDATE_SENT, params![id])?;
     Ok(())
@@ -122,16 +171,16 @@ async fn send_single_flag(
     server_url: &str,
     team_token: &str,
     flag: &Flag,
-) -> Result<reqwest::Response, reqwest::Error> {
+) -> Result<Response, reqwest::Error> {
     println!("[SEND] flag: {} group: {}", flag.flag, flag.group);
-    let client = reqwest::Client::new();
+    let client = Client::new();
     // Send team token and flag as post request
     let parameters = [("team_token", team_token), ("flag", &flag.flag)];
     let res = client.post(server_url).form(&parameters).send().await?;
     Ok(res)
 }
 
-async fn check_response(res: reqwest::Response) -> bool {
+async fn check_response(res: Response) -> bool {
     // Check if status is success and if response tells that flag is invalid (false positives)
     if res.status().is_success() {
         match res.text().await {
@@ -188,7 +237,7 @@ async fn send_flags_with_throttle(
     joins
 }
 
-async fn main_loop(db: &rusqlite::Connection, config: &Arc<Config>) {
+async fn main_loop(db: &Connection, config: &Arc<Config>) {
     // Interval for checking flags to sent
     let mut interval = interval(Duration::from_secs(config.check_interval as u64));
     // Set of all the sent flags
@@ -221,7 +270,7 @@ async fn main_loop(db: &rusqlite::Connection, config: &Arc<Config>) {
 #[tokio::main]
 async fn main() -> rusqlite::Result<()> {
     let config = config();
-    let db = rusqlite::Connection::open(&config.db_path)?;
+    let db = Connection::open(&config.db_path)?;
     setup(&db)?;
     main_loop(&db, &config).await;
     Ok(())
