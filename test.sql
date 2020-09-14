5e501ffa7bdb936a78e300d1b0d6b26a152e1c3f
Joshua Chapman <joshua.samuel.chapman@protonmail.com>
2020-09-14 20:36:04 +0200

    Added more logs and sqlite test

---
+++ test.sql
@@ -0,0 +1,5 @@
+INSERT INTO flags (flag, group_id) VALUES ('flag{AAAA}', 1);
+INSERT INTO flags (flag, group_id) VALUES ('flag{BBBB}', 2);
+INSERT INTO flags (flag, group_id) VALUES ('flag{CCCC}', 3);
+INSERT INTO flags (flag, group_id) VALUES ('flag{DDDD}', 4);
+.quit;
